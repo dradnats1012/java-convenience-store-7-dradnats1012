98e3f6657cdfabd20a5dc4529776cc8608884cd3
dradnats1012 <herjebi1012@gmail.com>
2024-11-09 23:59:27 +0900

    test: PromotionTest 수정

--- src/test/java/store/PromotionTest.java
+++ src/test/java/store/PromotionTest.java
@@ -4,25 +4,37 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
 
-import store.model.promotion.TwoPlusOnePromotion;
+import store.model.promotion.Promotion;
 
 public class PromotionTest {
 
     @Test
     void 프로모션_buy_숫자가_아니면_예외() {
-        assertThatThrownBy(() -> new TwoPlusOnePromotion("2+1", "이", "1", "2024-01-01", "2024-12-31"))
+        assertThatThrownBy(() -> new Promotion("2+1", "이", "1", "2024-01-01", "2024-12-31"))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 프로모션_get_숫자가_아니면_예외() {
-        assertThatThrownBy(() -> new TwoPlusOnePromotion("2+1", "2", "일", "2024-01-01", "2024-12-31"))
+        assertThatThrownBy(() -> new Promotion("2+1", "2", "일", "2024-01-01", "2024-12-31"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 프로모션_buy_음수면_예외() {
+        assertThatThrownBy(() -> new Promotion("2+1", "-2", "1", "2024-01-01", "2024-12-31"))
+            .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 프로모션_get_음수면_예외() {
+        assertThatThrownBy(() -> new Promotion("2+1", "2", "-1", "2024-01-01", "2024-12-31"))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
     @Test
     void 프로모션_Date_형식이_아니면_예외() {
-        assertThatThrownBy(() -> new TwoPlusOnePromotion("2+1", "2", "1", "2024-01-0111", "2024-12-31"))
+        assertThatThrownBy(() -> new Promotion("2+1", "2", "1", "2024-01-0111", "2024-12-31"))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
