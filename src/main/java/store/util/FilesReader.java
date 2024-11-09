e79c749690fe70d5939d870c1b30466379e24a86
dradnats1012 <herjebi1012@gmail.com>
2024-11-10 00:13:49 +0900

    feat: FilesReader 클래스 초기 구현

---
+++ src/main/java/store/util/FilesReader.java
@@ -0,0 +1,32 @@
+package store.util;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilesReader {
+
+    public static List<String> readProductsFile() {
+        List<String> products = new ArrayList<>();
+        try {
+            Path path = Path.of("src/main/resources/products.md");
+            products = Files.readAllLines(path);
+        } catch (IOException e) {
+            // outputview
+        }
+        return products;
+    }
+
+    public static List<String> readPromotionFile() {
+        List<String> promotions = new ArrayList<>();
+        try {
+            Path path = Path.of("src/main/resources/promotions.md");
+            promotions = Files.readAllLines(path);
+        } catch (IOException e) {
+            // outputview
+        }
+        return promotions;
+    }
+}
