646bd176608f7010ccdcbec6cac7f8ac3a3c589f
dradnats1012 <herjebi1012@gmail.com>
2024-11-11 03:38:19 +0900

    feat: Message 열거형 추가

--- src/main/java/store/util/Message.java
+++ src/main/java/store/util/Message.java
@@ -7,6 +7,15 @@ public enum Message {
     INPUT_IS_MORE_PURCHASE("감사합니다. 구매하고 싶은 다른 상품이 있나요? (Y/N)"),
     INPUT_NO_PROMOTION("현재 %s %d개는 프로모션 할인이 적용되지 않습니다. 그래도 구매하시겠습니까? (Y/N)"),
     INPUT_MORE_BENEFIT("현재 %s은(는) %d개를 무료로 더 받을 수 있습니다. 추가하시겠습니까? (Y/N)"),
+    HELLO_MESSAGE("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n"),
+    RECEIPT_HEADER("==============W 편의점================"),
+    RECEIPT_LIST_NAME("상품명\t\t수량\t금액"),
+    RECEIPT_LIST("%s\t\t%s\t%s"),
+    RECEIPT_BENEFIT("=============증\t정==============="),
+    RECEIPT_TOTAL_MONEY("총구매액\t\t%s\t%s"),
+    RECEIPT_PROMOTION_SALE("행사할인\t\t\t-%s"),
+    RECEIPT_MEMBERSHIP_SALE("멤버십할인\t\t\t-%s"),
+    RECEIPT_PAY_MONEY("내실돈\t\t\t%s")
     ;
 
 
