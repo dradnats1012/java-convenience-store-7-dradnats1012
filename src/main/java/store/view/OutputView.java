e5b394f3cccc85a533ffa757ac08f9ca9aa329cf
dradnats1012 <herjebi1012@gmail.com>
2024-11-11 06:59:34 +0900

    feat: OutputView 영수증 출력 메서드 구현

--- src/main/java/store/view/OutputView.java
+++ src/main/java/store/view/OutputView.java
@@ -1,13 +1,18 @@
 package store.view;
 
+import static store.util.Message.*;
+
+import store.model.shopping.ReceiptDTO;
 import store.model.store.StoreDTO;
+import store.util.Message;
 
 public class OutputView {
 
-    private OutputView(){}
+    private OutputView() {
+    }
 
-    public static void printReceipt(StoreDTO storeDTO){
-        System.out.println("안녕하세요. W편의점입니다.\n현재 보유하고 있는 상품입니다.\n");
+    public static void printMenu(StoreDTO storeDTO) {
+        System.out.println(Message.HELLO_MESSAGE.getMessage());
 
         storeDTO.products().forEach(productDTO ->
             System.out.printf("- %s %s원 %s %s%n",
@@ -17,5 +22,29 @@ public class OutputView {
                 productDTO.promotion()
             )
         );
+        System.out.println();
+    }
+
+    public static void printReceipt(ReceiptDTO receiptDTO) {
+        System.out.println(RECEIPT_HEADER.getMessage());
+        System.out.println(RECEIPT_LIST_NAME.getMessage());
+        receiptDTO.productDTOS().forEach(product ->
+            System.out.printf(RECEIPT_LIST.getMessage(), product.name(), product.quantity(), product.price())
+        );
+
+        // BenefitDTO 리스트 출력
+        System.out.println(RECEIPT_BENEFIT.getMessage());
+        receiptDTO.benefitDTOS().forEach(benefit ->
+            System.out.printf(RECEIPT_BENEFIT_LIST.getMessage(), benefit.name(), benefit.quantity())
+        );
+
+        // MoneyDTO 출력
+        System.out.println(RECEIPT_LINE.getMessage());
+        ReceiptDTO.MoneyDTO money = receiptDTO.moneyDTO();
+        System.out.printf(RECEIPT_TOTAL_MONEY.getMessage(), receiptDTO.totalQuantity(), money.totalMoney());
+        System.out.printf(RECEIPT_PROMOTION_SALE.getMessage(), money.promotionSaleMoney());
+        System.out.printf(RECEIPT_MEMBERSHIP_SALE.getMessage(), money.membershipSaleMoney());
+        System.out.printf(RECEIPT_PAY_MONEY.getMessage(), money.payMoney());
     }
 }
+
