6470bde06448e947fe78423edadcbe972cf68cfc
dradnats1012 <herjebi1012@gmail.com>
2024-11-11 03:36:38 +0900

    feat: Promotion 클래스 구현

--- src/main/java/store/model/promotion/Promotion.java
+++ src/main/java/store/model/promotion/Promotion.java
@@ -1,20 +1,51 @@
 package store.model.promotion;
 
 public class Promotion {
-    private String name;
-    private Buy buy;
-    private Get get;
-    private Period period;
+    private final String name;
+    private final Buy buy;
+    private final Get get;
+    private final Period period;
+    private final boolean isPeriod;
 
     public Promotion(String name, String buy, String get, String startDate, String endDate) {
         this.name = name;
         this.buy = new Buy(buy);
         this.get = new Get(get);
         this.period = new Period(startDate, endDate);
+        this.isPeriod = period.checkPeriod();
     }
 
-    public String getName(){
+    public String getName() {
         return name;
     }
 
+    public boolean getIsPeriod() {
+        return isPeriod;
+    }
+
+    public PromotionBenefitDTO purchase(int productQuantity, int purchaseQuantity) {
+        int total = buy.getBuy() + get.getGet();
+        int remain = purchaseQuantity % total;
+
+        if (purchaseQuantity >= total) {
+            handleFullBenefit(purchaseQuantity, total, remain);
+        }
+        if (purchaseQuantity >= buy.getBuy()) {
+            return PromotionBenefitDTO.of(0, get.getGet());
+        }
+
+        return PromotionBenefitDTO.of(0, 0);
+    }
+
+    private PromotionBenefitDTO handleFullBenefit(int purchaseQuantity, int total, int remain) {
+        int givenBenefit = (purchaseQuantity / total) * get.getGet();
+        if (remain < buy.getBuy()) {
+            return PromotionBenefitDTO.of(givenBenefit, 0);
+        }
+        return PromotionBenefitDTO.of(givenBenefit, remain - buy.getBuy() + 1);
+    }
+
+    private boolean isBenefit(int remain) {
+        return remain >= buy.getBuy();
+    }
 }
