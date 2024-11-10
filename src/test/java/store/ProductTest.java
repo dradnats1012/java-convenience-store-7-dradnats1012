b25baf1620a425234658dfdf12554a0a9e8bfae2
dradnats1012 <herjebi1012@gmail.com>
2024-11-11 00:54:13 +0900

    test: Product 클래스 변경에 따른 테스트 수정

--- src/test/java/store/ProductTest.java
+++ src/test/java/store/ProductTest.java
@@ -13,7 +13,7 @@ public class ProductTest {
     void 프로덕트_price_Integer_아니면_예외() {
         Promotion promotion = new Promotion("탄산2+1", "2", "1", "2024-01-01", "2024-12-31");
 
-        assertThatThrownBy(() -> new Product("콜라", "1000원", "10", promotion))
+        assertThatThrownBy(() -> new Product("콜라", "1000원", "10", "10", promotion))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -21,7 +21,7 @@ public class ProductTest {
     void 프로덕트_quantity_Integer_아니면_예외() {
         Promotion promotion = new Promotion("탄산2+1", "2", "1", "2024-01-01", "2024-12-31");
 
-        assertThatThrownBy(() -> new Product("콜라", "1000", "10개", promotion))
+        assertThatThrownBy(() -> new Product("콜라", "1000", "10개", "10", promotion))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -29,7 +29,7 @@ public class ProductTest {
     void 프로덕트_price_음수면_예외() {
         Promotion promotion = new Promotion("탄산2+1", "2", "1", "2024-01-01", "2024-12-31");
 
-        assertThatThrownBy(() -> new Product("콜라", "-1000", "10", promotion))
+        assertThatThrownBy(() -> new Product("콜라", "-1000", "10", "10", promotion))
             .isInstanceOf(IllegalArgumentException.class);
     }
 
@@ -37,7 +37,7 @@ public class ProductTest {
     void 프로덕트_quantity_음수면_예외() {
         Promotion promotion = new Promotion("탄산2+1", "2", "1", "2024-01-01", "2024-12-31");
 
-        assertThatThrownBy(() -> new Product("콜라", "1000", "-10", promotion))
+        assertThatThrownBy(() -> new Product("콜라", "1000", "-10", "10", promotion))
             .isInstanceOf(IllegalArgumentException.class);
     }
 }
--- src/test/java/store/PromotionTest.java
+++ src/test/java/store/PromotionTest.java
@@ -1,5 +1,7 @@
 package store;
 
+import static camp.nextstep.edu.missionutils.test.Assertions.assertSimpleTest;
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.AssertionsForClassTypes.assertThatThrownBy;
 
 import org.junit.jupiter.api.Test;
@@ -36,5 +38,21 @@ public class PromotionTest {
     void 프로모션_Date_형식이_아니면_예외() {
         assertThatThrownBy(() -> new Promotion("2+1", "2", "1", "2024-01-0111", "2024-12-31"))
             .isInstanceOf(IllegalArgumentException.class);
+    }
+
+    @Test
+    void 프로모션_기간_맞는지_확인() {
+        assertSimpleTest(() -> {
+            Promotion promotion = new Promotion("2+1", "2", "1", "2024-01-01", "2024-12-31");
+            assertThat(promotion.getIsPeriod()).isEqualTo(true);
+        });
+    }
+
+    @Test
+    void 프로모션_기간_아닌지_확인() {
+        assertSimpleTest(() -> {
+            Promotion promotion = new Promotion("2+1", "2", "1", "2023-01-01", "2023-12-31");
+            assertThat(promotion.getIsPeriod()).isEqualTo(false);
+        });
     }
 }
