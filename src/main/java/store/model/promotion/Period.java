3d7104121c9afe1976dc07a510d7e2402f041ede
dradnats1012 <herjebi1012@gmail.com>
2024-11-11 03:37:11 +0900

    feat: Period 클래스 구현

--- src/main/java/store/model/promotion/Period.java
+++ src/main/java/store/model/promotion/Period.java
@@ -1,7 +1,9 @@
 package store.model.promotion;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 
+import camp.nextstep.edu.missionutils.DateTimes;
 import store.util.Validator;
 
 public class Period {
@@ -9,8 +11,18 @@ public class Period {
     private final LocalDate startDate;
     private final LocalDate endDate;
 
-    public Period(String startDate, String endDate){
+    public Period(String startDate, String endDate) {
         this.startDate = Validator.validateDate(startDate);
         this.endDate = Validator.validateDate(endDate);
+    }
+
+    public boolean checkPeriod() {
+        LocalDateTime today = DateTimes.now();
+
+        if (today.isBefore(startDate.atStartOfDay()) || today.isAfter(endDate.atStartOfDay())) {
+            return false;
+        }
+
+        return true;
     }
 }
